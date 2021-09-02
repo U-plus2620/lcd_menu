9e0ceae67e455d6752964e81800927a1dde8a9bb
U-plus2620 <cooheneliezer@gmail.com>
2021-09-02 21:55:49 +0300

    Tuned up the dimming code and its interface display, moved around some variables to become globally accessable

--- My_LCD_test_Struct.c
+++ My_LCD_test_Struct.c
@@ -6,6 +6,7 @@
 #include <util/delay.h>
 #include "lcd.h"
 #include "menu.h"
+// #include <avr/interrupt.h>
 
 #define MILK   PD5
 #define MEAT   PD6
@@ -19,6 +20,15 @@ int DeBounce(int Pin_reg, int Pin){
     else return 0;
 }
 
+volatile uint8_t *dim_reg;
+uint8_t dimmer  = 0;
+void Dimmer(uint8_t dimmer,volatile uint8_t *dim_reg);
+
+uint8_t pointer = 0;
+item *current_menu;
+int current_menu_size;
+enum selection{milk,meat,RGB,on,off,dim,red,green,blue,white,back}selection;
+
 int main(void){ //Super loop
     
     //Pin configurations
@@ -27,12 +37,16 @@ int main(void){ //Super loop
     PORTD |=  (1 << DOWN);          /* Pin PD3 pull-up enabled    */
     PORTD |=  (1 << SELECT);          /* Pin PD4 pull-up enabled    */
     DDRD  |=  (1<<MILK) | (1<<MEAT); /*output pin*/   
-    PORTD &= ~(1<<MILK) | ~(1<<MEAT);/*output pin*/
-
-   
+    PORTD &= ~(1<<MILK) | ~(1<<MEAT); /*output pin*/
+    
+//    PCICR |= (1<<PCIE2);
+//    PCMSK2 |= (1<<PCINT18) | (1<<PCINT19) | (1<<PCINT20);
+    //Initialize timer for dimming
+    TCCR0A  |= (1<<WGM00) | (1<<WGM01); //(1<<COM0B1) | (1<<COM0A1) |
+    TCCR0B  |= (1<<CS00);
     
     // Various menus in the form of lists
-    char *main_menu[] = {"milk sink","meat sink","RGB's"};
+    char *main_menu[] = {"milk sink","meat sink","RGB's(INOP)"};
     int main_menu_size = sizeof(main_menu) / sizeof(main_menu[0]);
     item menuBuff[main_menu_size];
     int main_menu_actions[] = {0,1,2};
@@ -51,16 +65,6 @@ int main(void){ //Super loop
     int RGB_actions[] = {3,4,5,6,7,8,9,10}; 
     item *_RGB_menu = Menu_maker(RGB_menu,RGB_actions,RGB_size,RGBBuff);
     
-   
-   
-
-    enum selection{milk,meat,RGB,on,off,dim,red,green,blue,white,back}selection;
-
-    uint8_t pointer = 0;
-    item *current_menu;
-    int current_menu_size;
-
-
     //Initialization of the screen and main program
     lcd_init(LCD_DISP_ON);
     lcd_clrscr();
@@ -75,12 +79,10 @@ int main(void){ //Super loop
     lcd_puts("i help you?");
     _delay_ms(500 );
 
-   
-
     current_menu = _main_menu;
     current_menu_size = main_menu_size;
     Show(current_menu, pointer,current_menu_size);
-    
+
     while(1){
         if (DeBounce(PIND,UP)){ //Scroll up
             pointer = Scroll(1,0,pointer);
@@ -95,55 +97,102 @@ int main(void){ //Super loop
         selection = Select(current_menu,pointer,current_menu_size);
            switch(selection){
                 case milk:
+                    lcd_clrscr();
                     lcd_home();
-                    lcd_puts("milky sink");
-                    _delay_ms(700);
+                    _delay_ms(100);
                     current_menu = milky_sink_menu;
                     current_menu_size = sink_size;
                     Show(current_menu, pointer,current_menu_size);
                     break;
                 case meat:
+                    lcd_clrscr();
                     lcd_home();
-                    lcd_puts("meaty sink");
-                    _delay_ms(700);
+                    _delay_ms(100);
                     current_menu = meaty_sink_menu;
                     current_menu_size = sink_size;
                     Show(current_menu, pointer,current_menu_size);
                     break;
                 case RGB:
+                    lcd_clrscr();
                     lcd_home();
-                    lcd_puts("RGB's");
-                    _delay_ms(700);
+                    _delay_ms(100);
                     current_menu = _RGB_menu;
                     current_menu_size = RGB_size;
                     Show(current_menu, pointer,current_menu_size);
                     break;
                 case on:
                     if(current_menu == milky_sink_menu){
-                        PORTD |= (1<<MILK);
+                        TCCR0A |= (1<<COM0B1);
+                        OCR0B = 0xFF;
                     }
                     else if (current_menu == meaty_sink_menu){
-                   
-                        PORTD |= (1<<MEAT);
+                        TCCR0A |= (1<<COM0A1);
+                        OCR0A = 0xFF;
                     }
+                    // else if (current_menu == _RGB_menu){
+                    //      send on signal
+                    // }
                     break;
                 case off:
                      if(current_menu == milky_sink_menu){
-                        PORTD &= ~(1<<MILK);
+                        TCCR0A &= ~(1<<COM0B1);
+                        PORTD &= ~(1<< MILK);
                     }
                     else if (current_menu == meaty_sink_menu){
-                   
-                        PORTD &= ~(1<<MEAT);
+                       TCCR0A &= ~(1<<COM0A1);
+                       PORTD &= ~(1<< MEAT);
                     }
+                     // else if (current_menu == _RGB_menu){
+                    //      send off signal
+                    // }
                     break;
                 case dim:
+                        lcd_clrscr();
+                        char intbuff[10];
+                        itoa((dimmer/(float)0)*100,intbuff,10);
+                        lcd_puts("DIM\n");
+                        lcd_puts(intbuff);
+                        lcd_putc('%');
+                        do{
+                            if(DeBounce(PIND,UP)){
+                                dimmer += (dimmer!=255) ? 51 : 1; 
+                                itoa((dimmer/(float)255)*100,intbuff,10);
+                                lcd_clrscr();
+                                lcd_puts("DIM\n");
+                                lcd_puts(intbuff);
+                                lcd_putc('%');
+                                }
+                            else if (DeBounce(PIND,DOWN)){
+                                dimmer -= (dimmer!=0) ? 51 : 1; 
+                                itoa((dimmer/(float)255)*100,intbuff,10);
+                                lcd_clrscr();
+                                lcd_puts("DIM\n");
+                                lcd_puts(intbuff);
+                                lcd_putc('%');
+                                }
+                            if(current_menu == milky_sink_menu){
+                                OCR0B = dimmer;
+                                }
+                            else if (current_menu == meaty_sink_menu){
+                                OCR0A = dimmer;
+                                }
+                             // else if (current_menu == _RGB_menu){
+                             //      send dim signal
+                             // }
+                        }while (!DeBounce(PIND, SELECT));
+                        Show(current_menu,pointer,current_menu_size);
                     break;
                 case red:
+                    lcd_puts("INOP");
                     break;
                 case green:
+                    lcd_puts("INOP");
                     break;
                 case blue:
+                    lcd_puts("INOP");
                     break;
+                case white:
+                    lcd_puts("INOP");
                 case back:
                     current_menu = _main_menu;
                     current_menu_size = main_menu_size;
@@ -151,7 +200,6 @@ int main(void){ //Super loop
                 default:
                     break;
                 }
-            }
-        
+        }   
     }
 }
